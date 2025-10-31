bce58b610adbaba6e526ac5bb70dfac8a9cac368
Ricardo Minguez Pablos (RIDO) <rmpablos@microsoft.com>
2025-10-31 15:25:30 -0700

    test agentic tokens

---
+++ docs/Tokens.md
@@ -0,0 +1,53 @@
+# Tokens
+
+## Incoming
+
+Incoming Bot Token
+
+```
+{
+  "aud": "https://api.botframework.com",
+  "iss": "https://sts.windows.net/56653e9d-2158-46ee-90d7-675c39642038/",
+  "iat": 1761841803,
+  "nbf": 1761841803,
+  "exp": 1761845703,
+  "aio": "k2JgYNi6tPPvsYW3Hy+WXa/EwfsyEwA=",
+  "appid": "aabdbd62-bc97-4afb-83ee-575594577de5",
+  "appidacr": "1",
+  "idp": "https://sts.windows.net/56653e9d-2158-46ee-90d7-675c39642038/",
+  "idtyp": "app",
+  "oid": "9fd99b86-fb5d-4a14-912c-847a9638c8ea",
+  "rh": "1.AW8BnT5lVlgh7kaQ12dcOWQgOEIzLY0pz1lJlXcODq-9FrznAQBvAQ.",
+  "sub": "9fd99b86-fb5d-4a14-912c-847a9638c8ea",
+  "tid": "56653e9d-2158-46ee-90d7-675c39642038",
+  "uti": "w4ELbvVE1kW4zAe6ZN8sAA",
+  "ver": "1.0",
+  "xms_act_fct": "3 9",
+  "xms_ftd": "3Y0d_0C7vdXnyrd53tHHwX2jWqr_yHgBt8fDtzzwviIBdXN3ZXN0My1kc21z",
+  "xms_idrel": "7 14",
+  "xms_rd": "0.42LjYBJies8kJMLBLiTwrnx-c-KUR17zmUI3vC2dmAcU5RQSYEADQFEOIQEnY91ezfORnlPL-fjW7xXbAwA",
+  "xms_sub_fct": "3 9"
+}
+```
+
+Incoming AU token
+
+```
+{
+  "aud": "f8c0696d-bb99-4bb4-acc9-47c734d91e91",
+  "iss": "https://login.microsoftonline.com/e8a85347-fb53-4a91-9267-c616cbe1fd16/v2.0",
+  "iat": 1761886755,
+  "nbf": 1761886755,
+  "exp": 1761973455,
+  "aio": "ASQA2/8aAAAACfR4Cpbb1duSuKh++07IGImffePIqheimDdDlGyS4bQ=",
+  "azp": "5a807f24-c9de-44ee-a3a7-329e88a00ffc",
+  "azpacr": "2",
+  "oid": "86d5c138-b33a-43f7-805f-59f75e4c699b",
+  "rh": "1.AH4AR1Oo6FP7kUqSZ8YWy-H9Fm1pwPiZu7RLrMlHxzTZHpF-AAB-AA.",
+  "sub": "86d5c138-b33a-43f7-805f-59f75e4c699b",
+  "tid": "e8a85347-fb53-4a91-9267-c616cbe1fd16",
+  "uti": "sZurb2dQBEaWTurnDM6AAA",
+  "ver": "2.0",
+  "xms_ftd": "jaieST26R6fFYuFoJnyJDS_0_Dgjw7CFSSd5QPpH4gsBdXN3ZXN0My1kc21z"
+}
+```
--- samples/AgenticTokens/Program.cs
+++ samples/AgenticTokens/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.Identity.Abstractions;
 using Microsoft.Identity.Web;
 using Microsoft.Identity.Web.TokenCacheProviders.InMemory;
@@ -5,47 +6,56 @@ using System.Security.Claims;
 
 Microsoft.IdentityModel.Logging.IdentityModelEventSource.ShowPII = true;
 
-string agentScope = "0d94caae-b412-4943-8a68-83135ad6d35f/.default";
-string tenantId = "5369a35c-46a5-4677-8ff9-2e65587654e7";
-string clientId = "9d17cc32-c91b-4368-8494-1b29ccb0dbcf";
+string agentScope = "5a807f24-c9de-44ee-a3a7-329e88a00ffc/.default";
+string tenantId = "e8a85347-fb53-4a91-9267-c616cbe1fd16";
+string clientId = "f8c0696d-bb99-4bb4-acc9-47c734d91e91";
 //string miClientId = "a8c6bf8e-d4a0-4181-b91d-501314cda52a";
-string agentIdentity = "5081ddac-3d33-4766-98fe-80c38c5ce554";
-string userId = "96a64abd-3267-4143-b9a5-194e6f96ef2b";
+string agentIdentity = "f30805e3-3457-4c6e-a0e7-bf0fd623f887";
+string userId = "715d0396-3a7a-4d44-800d-225d04e4d510";
 
-var builder = new HostApplicationBuilder(args);
-
+var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddTokenAcquisition(true);
 builder.Services.AddAgentIdentities();
 builder.Services.AddInMemoryTokenCaches();
 builder.Services.AddHttpClient();
-string secret = builder.Configuration["MicrosoftAppPassword"]!;
-builder.Services.Configure<MicrosoftIdentityApplicationOptions>(ops =>
-{
-    ops.Instance = "https://login.microsoftonline.com/";
-    ops.TenantId = tenantId;
-    ops.ClientId = clientId;
-    ops.ClientCredentials = [
-        new CredentialDescription()
-        {
-            //SourceType = CredentialSource.SignedAssertionFromManagedIdentity,
-            //ManagedIdentityClientId = miClientId
-            SourceType = CredentialSource.ClientSecret,
-            ClientSecret = secret 
-        }
-    ];
-});
+
+builder.Services.Configure<MicrosoftIdentityApplicationOptions>(builder.Configuration.GetSection("AzureAd"));
+
+// string secret = builder.Configuration["MicrosoftAppPassword"]!;
+//builder.Services.Configure<MicrosoftIdentityApplicationOptions>(ops =>
+//{
+//    ops.Instance = "https://login.microsoftonline.com/";
+//    ops.TenantId = tenantId;
+//    ops.ClientId = clientId;
+//    ops.ClientCredentials = [
+//        new CredentialDescription()
+//        {
+//            //SourceType = CredentialSource.SignedAssertionFromManagedIdentity,
+//            //ManagedIdentityClientId = miClientId
+//            SourceType = CredentialSource.ClientSecret,
+//            ClientSecret = secret 
+//        }
+//    ];
+//});
 var app = builder.Build();
 
 string[] scopes = new[] { agentScope };
 
 IAuthorizationHeaderProvider authorizationHeaderProvider = app.Services.GetRequiredService<IAuthorizationHeaderProvider>();
-AuthorizationHeaderProviderOptions options = new AuthorizationHeaderProviderOptions().WithAgentUserIdentity(agentIdentity, Guid.Parse(userId));
+AuthorizationHeaderProviderOptions options = new AuthorizationHeaderProviderOptions()
+    .WithAgentUserIdentity(agentIdentity, Guid.Parse(userId));
 
 ClaimsPrincipal user = new ClaimsPrincipal();
 
 string authHeader = await authorizationHeaderProvider
-    .CreateAuthorizationHeaderAsync(scopes,options,cancellationToken: default);
+    .CreateAuthorizationHeaderAsync(scopes,options,user, cancellationToken: default);
 
 
 user.Claims.ToList().ForEach(c => System.Console.WriteLine($"{c.Type}: {c.Value}"));
+
+Console.WriteLine(  );
+
+var jsonToken = new Microsoft.IdentityModel.JsonWebTokens.JsonWebToken(authHeader["Bearer ".Length..]);
+jsonToken.Claims.ToList().ForEach(c => System.Console.WriteLine($"{c.Type}: {c.Value}"));
+
 System.Console.WriteLine(authHeader);
