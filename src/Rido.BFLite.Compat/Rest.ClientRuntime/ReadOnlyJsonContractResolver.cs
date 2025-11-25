ec1f63438d1683d265786b72e9dd999cab1c029d
Ricardo Minguez Pablos (RIDO) <rmpablos@microsoft.com>
2025-11-24 19:55:20 -0800

    add missing types for user token

---
+++ src/Rido.BFLite.Compat/Rest.ClientRuntime/ReadOnlyJsonContractResolver.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.Rest.Serialization
+{
+    public class ReadOnlyJsonContractResolver : DefaultContractResolver
+    {
+        protected override JsonProperty CreateProperty(MemberInfo member, MemberSerialization memberSerialization)
+        {
+            JsonProperty jsonProperty = base.CreateProperty(member, memberSerialization);
+            PropertyInfo propertyInfo = member as PropertyInfo;
+            if (propertyInfo != null)
+            {
+                jsonProperty.ShouldSerialize = (object t) => (propertyInfo.SetMethod != null && !propertyInfo.SetMethod.IsPrivate && !propertyInfo.SetMethod.IsFamily) || (propertyInfo.GetMethod != null && propertyInfo.GetMethod.IsStatic);
+            }
+
+            return jsonProperty;
+        }
+    }
+}
