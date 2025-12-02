3953c284df63dbcd4ca9db8b9ccc52a36ac001d7
Ricardo Minguez Pablos (RIDO) <rmpablos@microsoft.com>
2025-12-01 16:39:07 -0800

    test from echo bot

--- samples/Samples.Compat/EchoBot.cs
+++ samples/Samples.Compat/EchoBot.cs
@@ -1,6 +1,8 @@
 using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Teams;
 using Microsoft.Bot.Connector.Authentication;
 using Microsoft.Bot.Schema;
+using Microsoft.Bot.Schema.Teams;
 using System.Collections.Concurrent;
 
 class EchoBot(ConcurrentDictionary<string, ConversationReference> conversationReferences) : ActivityHandler
@@ -17,6 +19,12 @@ class EchoBot(ConcurrentDictionary<string, ConversationReference> conversationRe
         conversationReferences.AddOrUpdate(convRef.User.Id, convRef, (k, v) => convRef);
         var replyText = $"Echo: {turnContext.Activity.Text}";
         await turnContext.SendActivityAsync(MessageFactory.Text(replyText, replyText), cancellationToken);
+
+
+        var members = await TeamsInfo.GetMembersAsync(turnContext)!;
+
+        await turnContext.SendActivityAsync("members " + string.Join(", ", members.ToList().Select(m => m.Name)));
+
 
         UserTokenClient userTokenClient = turnContext.TurnState.Get<UserTokenClient>();
         var tokenResponse = await userTokenClient.GetTokenStatusAsync(turnContext.Activity.From.Id, turnContext.Activity.ChannelId, null, cancellationToken);
