b470fba8f9026998690c29ee834ee75d5092c9cf
Rido <rido-min@users.noreply.github.com>
2025-12-08 13:50:01 -0800

    AddCompat (#29)

---
+++ src/Rido.BFLite.Compat.Adapter/CompatHostingExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Hosting;
+using Rido.BFLite.Core.Hosting;
+using Rido.BFLite.Core;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Bot.Builder.Integration.AspNet.Core;
+
+namespace Rido.BFLite.Compat.Adapter;
+
+public static class CompatHostingExtensions
+{
+    public static IHostApplicationBuilder AddCompatAdapter(this IHostApplicationBuilder builder)
+    {
+        builder.Services.AddBotApplication<BotApplication>();
+        builder.Services.AddSingleton<CompatBotAdapter>();
+        builder.Services.AddSingleton<IBotFrameworkHttpAdapter, CompatAdapter>();
+        return builder;
+    }
+}
